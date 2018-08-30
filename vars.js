f0a3ddad72a0c566cc47e137ca1ad8c2d1fbd47f
1jjanata <42749702+1jjanata@users.noreply.github.com>
2018-08-30 13:58:51 -0500

    Add files via upload

---
+++ vars.js
@@ -0,0 +1,26 @@
+let a = 10;
+let b = -25
+let c = 5
+let d = 17 
+//create two more variables c and d and give them whatever number you want.
+
+
+const total = a+b+c+d;
+console.log(total);
+
+
+
+
+//new section on string variables
+let convo = "hello ";
+//set name to your name, first and last.
+let name = Jacob Janata
+//add string convo and name together.
+const greeting =
+console.log(greeting);
+
+//make your own message similar to what is above.
+let convo2;
+let
+const
+console.log();
